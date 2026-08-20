9d9eb4a469d5578f4e81c625adb9758db4afe463
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-08-20 05:17:06 -0400

    Update in.py [skip ci]

--- in.py
+++ in.py
@@ -1,5 +1,4 @@
-sudo apt-get update 
-sudo apt-get install -y --no-install-recommends \
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
   libegl1 libgl1 libglib2.0-0 libxkbcommon0 libdbus-1-3 libfontconfig1 \
   libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 \
   libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 \
